af662282f3877ea6ed98a4563b03648ec7e3f512
Vincentius Kennedy Winardinata <kenken010103@gmail.com>
2023-12-11 21:28:37 +0800

    Add Textfields UI with Reverse and Delete Button

--- app/src/main/java/com/cafealyzer/cafealyzer/ui/component/topcafescreen/BottomBar.kt
+++ app/src/main/java/com/cafealyzer/cafealyzer/ui/component/topcafescreen/BottomBar.kt
@@ -1,10 +1,5 @@
 package com.cafealyzer.cafealyzer.ui.component.topcafescreen
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -12,9 +7,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.cafealyzer.cafealyzer.R
 import com.cafealyzer.cafealyzer.ui.navigation.NavigationItem
 import com.cafealyzer.cafealyzer.ui.navigation.Screen
 
@@ -30,22 +27,23 @@ fun BottomBar(
         val currentRoute = navBackStackEntry?.destination?.route
         val navigationItems = listOf(
             NavigationItem(
-                title = "Peta", icon = Icons.Default.Home, screen = Screen.Maps
+                title = "Peta", icon = R.drawable.ic_home, screen = Screen.Maps
             ),
             NavigationItem(
-                title = "Top Cafe", icon = Icons.Default.ThumbUp, screen = Screen.TopCafe
+                title = "Top Cafe", icon = R.drawable.ic_thumbup, screen = Screen.TopCafe
             ),
             NavigationItem(
-                title = "Riwayat", icon = Icons.Default.Refresh, screen = Screen.History
+                title = "Riwayat", icon = R.drawable.ic_history, screen = Screen.History
             ),
             NavigationItem(
-                title = "Profil", icon = Icons.Default.Person, screen = Screen.Profile
+                title = "Profil", icon = R.drawable.ic_person, screen = Screen.Profile
             ),
         )
         navigationItems.map { item ->
             NavigationBarItem(icon = {
                 Icon(
-                    imageVector = item.icon, contentDescription = item.title
+                    painter = painterResource(id = item.icon),
+                    contentDescription = item.title
                 )
             }, label = {
                 Text(item.title)
--- app/src/main/java/com/cafealyzer/cafealyzer/ui/screen/MapScreen.kt
+++ app/src/main/java/com/cafealyzer/cafealyzer/ui/screen/MapScreen.kt
@@ -4,12 +4,14 @@ import android.content.ContentValues.TAG
 import android.content.pm.PackageManager
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,9 +20,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cafealyzer.cafealyzer.R
 import com.cafealyzer.cafealyzer.ui.activity.MainActivity
 import com.cafealyzer.cafealyzer.ui.component.mapscreen.SearchBar
 import com.cafealyzer.cafealyzer.ui.viewmodel.MapsViewModel
@@ -86,46 +91,95 @@ fun MapsScreen() {
             PERMISSION_REQUEST_CODE
         )
     }
+    var cafeAnda by remember { mutableStateOf("") }
+    var cafeKompetitor by remember { mutableStateOf("") }
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "Pilih Cafe Anda dan Kompetitor")
-                }
+    fun reverseCafe() {
+        val temp = cafeAnda
+        cafeAnda = cafeKompetitor
+        cafeKompetitor = temp
+    }
+
+    Column {
+        var searchText by remember { mutableStateOf("") }
+
+        SearchBar(
+            query = searchText,
+            onQueryChange = {
+                searchText = it
+            },
+            onSearchPerformed = {
+                Log.d("Search", "Search performed with query: $searchText")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            TextField(
+                value = cafeAnda,
+                onValueChange = { cafeAnda = it },
+                label = { Text("Cafe Anda") },
+                modifier = Modifier
+                    .weight(1f),
+                readOnly = false
             )
-        },
-        content = { innerpadding ->
-            Column {
-                var searchText by remember { mutableStateOf("") }
-
-                SearchBar(
-                    query = searchText,
-                    onQueryChange = {
-                        searchText = it
-                    },
-                    onSearchPerformed = {
-                        Log.d("Search", "Search performed with query: $searchText")
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(innerpadding)
+            IconButton(
+                onClick = {
+                    cafeAnda = ""
+                    cafeKompetitor = ""
+                },
+                modifier = Modifier.padding(4.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_delete),
+                    contentDescription = null,
+                    tint = Color.Gray
                 )
-
-                GoogleMap(
-                    cameraPositionState = cameraPositionState,
-                ) {
-                    MarkerInfoWindowContent(
-                        state = MarkerState(
-                            position = deviceLatLng
-                        )
-                    ) { marker ->
-                        Text(marker.title ?: "You", color = Color.Red)
-                    }
-                }
             }
         }
-    )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            TextField(
+                value = cafeKompetitor,
+                onValueChange = { cafeKompetitor = it },
+                label = { Text("Cafe Kompetitor") },
+                modifier = Modifier
+                    .weight(1f),
+                readOnly = false
+            )
+            IconButton(
+                onClick = { reverseCafe() },
+                modifier = Modifier.padding(4.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_reverse),
+                    contentDescription = null,
+                    tint = Color.Gray
+                )
+            }
+        }
+        GoogleMap(
+            cameraPositionState = cameraPositionState,
+        ) {
+            MarkerInfoWindowContent(
+                state = MarkerState(
+                    position = deviceLatLng
+                )
+            ) { marker ->
+                Text(marker.title ?: "You", color = Color.Red)
+            }
+        }
+    }
 }
 
 private const val PERMISSION_REQUEST_CODE = 1001
